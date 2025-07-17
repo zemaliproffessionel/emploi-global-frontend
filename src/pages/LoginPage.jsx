92f006c615a91097f76ef55aabb414ea98ef38e1
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:44:10 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm';
 
 const LoginPage = () => {
   return (
     <div className="container mx-auto py-12">
-      <h1 className="text-3xl font-bold text-center">Page de Connexion</h1>
-      {/* Le formulaire de connexion sera ajouté ici */}
+      <LoginForm />
     </div>
   );
 };
