7d8ac0404aac37a85a6189a43630b385a19b3223
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:20:54 +0100

    Create RegisterPage.jsx

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div className="container mx-auto py-12">
+      <h1 className="text-3xl font-bold text-center">Page d'Inscription</h1>
+      {/* Le formulaire d'inscription sera ajouté ici */}
+    </div>
+  );
+};
+
+export default RegisterPage;
