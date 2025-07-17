2734ba7a5a6f742bc5849a7a26f25df0ed7a4071
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:47:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { AuthProvider } from './context/AuthContext.jsx'; // On importe notre fournisseur
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* On enveloppe App avec AuthProvider */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 );
