2b2b14a8043ee4be155d229f641592cbf1a85b9c
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:20:25 +0100

    Create LoginPage.jsx

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div className="container mx-auto py-12">
+      <h1 className="text-3xl font-bold text-center">Page de Connexion</h1>
+      {/* Le formulaire de connexion sera ajouté ici */}
+    </div>
+  );
+};
+
+export default LoginPage;
