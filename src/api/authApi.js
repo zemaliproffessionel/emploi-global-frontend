96bf73d5fdcacc1902cdfc9e2eb56db0fd18d453
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:45:03 +0100

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
-// L'adresse de notre backend. Pour l'instant, c'est une adresse locale.
-// Plus tard, ce sera l'adresse de notre serveur en ligne.
 const API_URL = 'http://localhost:5000/api/users/';
 
-// Fonction pour appeler la route d'inscription du backend
-const register = (email, password ) => {
+// Fonction pour l'inscription (inchangée )
+const register = (email, password) => {
   return axios.post(API_URL + 'register', {
+    email,
+    password,
+  });
+};
+
+// NOUVELLE fonction pour la connexion
+const login = (email, password) => {
+  return axios.post(API_URL + 'login', {
     email,
     password,
   });
@@ -14,6 +20,7 @@ const register = (email, password ) => {
 
 const authApi = {
   register,
+  login, // On ajoute la nouvelle fonction à notre objet
 };
 
 export default authApi;
