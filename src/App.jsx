90bfa7ea208b445554156909c180b4503d7d573a
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-18 00:05:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage'; // <-- LIGNE AJOUTÉE
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} /> {/* <-- LIGNE AJOUTÉE */}
           </Routes>
         </main>
 
