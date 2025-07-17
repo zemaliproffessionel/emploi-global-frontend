59871f337d4286f9b91dda72b07b7e967ecdc695
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:19:57 +0100

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import FeaturedJobs from '../components/FeaturedJobs';
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedJobs />
+    </>
+  );
+};
+
+export default HomePage;
