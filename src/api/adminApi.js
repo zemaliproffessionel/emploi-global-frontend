5ce595d62169f5890535acaabf5a74a7f96c708f
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-18 13:39:56 +0100

    Create adminApi.js

---
+++ src/api/adminApi.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// L'adresse de notre backend pour les paiements
+const API_URL = 'http://localhost:5000/api/payments/';
+
+// Fonction pour récupérer tous les paiements
+const getPayments = ( ) => {
+  // Plus tard, on ajoutera le token de l'admin ici
+  return axios.get(API_URL);
+};
+
+// Fonction pour valider un paiement
+const validatePayment = (paymentId) => {
+  return axios.put(API_URL + `${paymentId}/validate`);
+};
+
+const adminApi = {
+  getPayments,
+  validatePayment,
+};
+
+export default adminApi;
