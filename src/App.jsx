2db84b12da45d54b6a2cb8217a5dfe2912847ef9
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-18 11:08:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
-import SearchPage from './pages/SearchPage'; // <-- LIGNE AJOUTÉE
+import SearchPage from './pages/SearchPage';
+import JobDetailPage from './pages/JobDetailPage'; // On importe la nouvelle page
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/search" element={<SearchPage />} /> {/* <-- LIGNE AJOUTÉE */}
+            <Route path="/search" element={<SearchPage />} />
+            {/* La nouvelle route pour les détails d'une offre */}
+            <Route path="/job/:id" element={<JobDetailPage />} /> 
           </Routes>
         </main>
 
