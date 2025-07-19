948f124e997e57a757d153945f25613ebc355eb8
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-19 01:03:56 +0100

    Update jobApi.js

--- src/api/jobApi.js
+++ src/api/jobApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // L'adresse de notre backend
-const API_URL = 'http://localhost:5000/api/jobs/';
+const API_URL = 'https://emploi-global-backend.onrender.com/api/jobs/';
 
 // Fonction pour récupérer toutes les offres (avec filtres optionnels )
 const getAllJobs = (params = {}) => {
