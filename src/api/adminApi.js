ac6fc2b35e82a44a987bb8cc00a94a6e3255e0cb
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-19 01:04:26 +0100

    Update adminApi.js

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // L'adresse de notre backend pour les paiements
-const API_URL = 'http://localhost:5000/api/payments/';
-
+const API_URL = 'https://emploi-global-backend.onrender.com/api/payments/';
 // Fonction pour récupérer tous les paiements
 const getPayments = ( ) => {
   // Plus tard, on ajoutera le token de l'admin ici
