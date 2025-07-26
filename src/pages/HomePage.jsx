2a2b2f426bd19c4c5b1ace7eb4590faf1bc64841
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-26 21:55:12 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import FeaturedJobs from '../components/FeaturedJobs';
+import PricingSection from '../components/PricingSection'; // Nous allons créer ce composant juste après
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <FeaturedJobs />
+      <PricingSection />
     </>
   );
 };
