b7564a387faa6da96eb2057cbffee12c94ec7342
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-08-02 13:30:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react( )],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://emploi-global-backend.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
