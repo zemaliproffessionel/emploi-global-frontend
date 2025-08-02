71920f044ce3e685ef4da153a9919fe7cf355ac8
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-08-02 13:28:52 +0100

    Update FeaturedJobs.jsx

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -25,7 +25,7 @@ const FeaturedJobs = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {jobs.map(job => (
-            <JobCard key={job.id} job={job} />
+            <JobCard key={job.id} id={job.id} title={job.title} company={job.company} location={job.location} />
           ))}
         </div>
       </div>
